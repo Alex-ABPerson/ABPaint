91dbc2f0628233be3f6636f23c3eee55074199ab
Alex-TIMEHACK <alexbu2015@hotmail.com>
2017-12-16 22:12:00 +0000

    Fixed build errors!

--- ABPaint/Properties/Resources.Designer.cs
+++ ABPaint/Properties/Resources.Designer.cs
@@ -183,16 +183,6 @@ namespace ABPaint.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Open1 {
-            get {
-                object obj = ResourceManager.GetObject("Open1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap PaintBrush {
             get {
                 object obj = ResourceManager.GetObject("PaintBrush", resourceCulture);
